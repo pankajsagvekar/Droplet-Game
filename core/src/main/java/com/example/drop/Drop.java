af1d0674e359cbacbfc92cac93a7b753d8f70ce3
Pankaj Sagvekar <pankajsagvekar167@gmail.com>
2024-08-29 20:52:23 +0530

    load assets and music

--- core/src/main/java/com/example/drop/Drop.java
+++ core/src/main/java/com/example/drop/Drop.java
@@ -2,6 +2,7 @@ package com.example.drop;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,26 +10,20 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Drop extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+    private Texture dropImage;
+    private Texture bucketImage;
+    private Music rainMusic;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
-    }
+        //load image
+        dropImage = new Texture(Gdx.files.internal("droplet.png")); 
+        bucketImage = new Texture(Gdx.files.internal("bucket.png")); 
 
-    @Override
-    public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
-    }
+        //load music
+        rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
 
-    @Override
-    public void dispose() {
-        batch.dispose();
-        image.dispose();
-    }
+        rainMusic.setLooping(true);
+        rainMusic.play();
+    } 
 }
